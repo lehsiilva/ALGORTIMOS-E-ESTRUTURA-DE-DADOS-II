4c5734e61e9dff709fa9d2437992d8a91b6a70dc
lehsiilva <leticiabeatrizlopes@hotmail.com>
2025-08-26 22:04:00 -0300

    Verificação

--- TPs/TP02/Invertido.java
+++ TPs/TP02/Invertido.java
@@ -1,17 +1,33 @@
-/*Crie um método iterativo que recebe uma string como parâmetro e retorna a string invertida. Na saída padrão, para cada linha de entrada, escreva uma linha de saída com a string invertida. Por exemplo, se a entrada for abcde, a saída deve ser edcba. */
 import java.util.Scanner;
 
 public class Invertido {
+
+    public static Boolean verificaFim(String palavra, String fimPalavra){
+        
+        boolean fim = false;
+
+        if(palavra.length() == fimPalavra.length()){ // Verificar tamanho para comparação
+            fim = true;
+            for(int i = 0; i < fimPalavra.length(); i ++){
+                if(palavra.charAt(i) != fimPalavra.charAt(i)){ //Se houver qualquer coisa diferente já é falso
+                    fim = false;
+                    i = fimPalavra.length();//Parar contador
+                }
+            }
+        }
+
+        return fim;
+    }
 
     public static String invertido(String palavra){
 
         int tam = palavra.length() - 1;
 
-        StringBuilder novaPalavra = new StringBuilder(palavra);
+        StringBuilder novaPalavra = new StringBuilder(palavra);// Permite alterar cada posição da String
 
         for(int i = 0; i < palavra.length()/2; i++){
             char temp = novaPalavra.charAt(i);
-            novaPalavra.setCharAt(i,novaPalavra.charAt(tam));
+            novaPalavra.setCharAt(i,novaPalavra.charAt(tam)); //Verificar se pode 
             novaPalavra.setCharAt(tam,temp);
             tam--;
         }
@@ -23,7 +39,7 @@ public class Invertido {
 
             String palavra = scanner.nextLine();
 
-            while(!palavra.equals("FIM")){
+            while(verificaFim(palavra,"FIM") != true){
                 System.out.println(invertido(palavra));
                 palavra = scanner.nextLine();
             }
