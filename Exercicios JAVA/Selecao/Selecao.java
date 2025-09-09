3959e0b6b27736da3deb0b5e08658bf2beed6007
lehsiilva <leticiabeatrizlopes@hotmail.com>
2025-09-09 20:51:45 -0300

    Concluído

---
+++ Exercicios JAVA/Selecao/Selecao.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Selecao {
+
+    public static void imprimir(int[]array,int n){
+        for(int i = 0; i < n; i++){
+            System.out.print(array[i] + "-");
+        }
+    }
+
+    public static void swap(int[]array, int menor, int i){
+        int temp = array[menor];
+        array[menor] = array[i];
+        array[i] = temp;
+
+    }
+
+    public static void select(int[] array, int n){
+        for(int i = 0; i < (n-1); i++){
+            int menor = i;
+            for(int j = i+1; j < n; j++){
+                if(array[menor] > array[j]){
+                    menor = j;
+                }
+            }
+            swap(array,menor,i);
+        }
+
+        imprimir(array,n);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] array={2,5,4,1,3};
+
+        select(array,n);
+
+        scanner.close();
+    }
+}
