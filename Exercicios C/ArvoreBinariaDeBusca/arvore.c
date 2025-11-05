ac87c01ff61c902c07ef1555b8f08de2148d4b98
lehsiilva <leticiabeatrizlopes@hotmail.com>
2025-11-05 06:42:25 -0300

    Correções

--- Exercicios C/ArvoreBinariaDeBusca/arvore.c
+++ Exercicios C/ArvoreBinariaDeBusca/arvore.c
@@ -3,70 +3,56 @@
 #include <stdbool.h>
 
 typedef struct No{
-   int valor;
-   struct No *dir;
-   struct No *esq;
-}No;
-
+    int valor;
+    struct No *dir;
+    struct No *esq;
+} No;
 
 typedef struct Arvore{
-    struct No *raiz;
-}Arvore;
-
+    No *raiz;
+} Arvore;
 
 Arvore *CriarArvore(){
-    Arvore *arvore = (Arvore*)malloc(sizeof(Arvore));
+    Arvore *arvore = (Arvore*) malloc(sizeof(Arvore));
     arvore->raiz = NULL;
-
     return arvore;
 }
 
-No *criarNo(int valor) {
-    No* novo = (No*) malloc(sizeof(No));
+No *criarNo(int valor){
+    No *novo = (No*) malloc(sizeof(No));
     novo->valor = valor;
     novo->esq = NULL;
     novo->dir = NULL;
     return novo;
 }
 
-void inserir(Arvore *arvore, int x){
-    arvore->raiz = inserirRec(arvore->raiz,x);
-}
-
-
-No *inserirRec(No *raiz,int x){
+No *inserirRec(No *raiz, int x){
     if(raiz == NULL){
-        Arvore *nova = (Arvore*)malloc(sizeof(Arvore));
-        return nova;
+        return criarNo(x);
     }
 
-    if(x > raiz->valor){
-        return inserirRec(raiz->dir, x);
-    }else if(x < raiz->valor){
-        return inserirRec(raiz->esq, x);
+    if(x < raiz->valor){
+        raiz->esq = inserirRec(raiz->esq, x);
+    } else if(x > raiz->valor){
+        raiz->dir = inserirRec(raiz->dir, x);
     }
 
     return raiz;
 }
 
-bool pesquisar(Arvore *arvore, int x){
-    return pesquisaRec(arvore->raiz, x);
+void inserir(Arvore *arvore, int x){
+    arvore->raiz = inserirRec(arvore->raiz, x);
 }
 
 bool pesquisaRec(No *raiz, int x){
-    bool resp = false;
+    if(raiz == NULL) return false;
+    if(x == raiz->valor) return true;
+    if(x < raiz->valor) return pesquisaRec(raiz->esq, x);
+    return pesquisaRec(raiz->dir, x);
+}
 
-    if(x == raiz->valor){
-        resp = true;
-    }
-
-    if(x < raiz->valor){
-        return pesquisaRec(raiz->esq, x);
-    }else if(x > raiz->valor){
-        return pesquisaRec(raiz->dir, x);
-    }
-
-    return resp;
+bool pesquisar(Arvore *arvore, int x){
+    return pesquisaRec(arvore->raiz, x);
 }
 
 int maiorEsq(No *raiz){
@@ -74,43 +60,37 @@ int maiorEsq(No *raiz){
     while(i->dir != NULL){
         i = i->dir;
     }
-
-    return i;
-}
-
-void remover(Arvore *arvore, int x){
-    return removerRec(arvore->raiz, x);
+    return i->valor;
 }
 
 No *removerRec(No *raiz, int x){
-    if(x > raiz->valor){
-        return inserirRec(raiz->dir, x);
-    }else if(x < raiz->valor){
-        return inserirRec(raiz->esq, x);
-    }else{
+    if(raiz == NULL) return NULL;
 
+    if(x > raiz->valor){
+        raiz->dir = removerRec(raiz->dir, x);
+    } else if(x < raiz->valor){
+        raiz->esq = removerRec(raiz->esq, x);
+    } else {
         if(raiz->dir == NULL && raiz->esq == NULL){
             free(raiz);
             return NULL;
         }
-
         if(raiz->dir == NULL){
             No *temp = raiz->esq;
             free(raiz);
             return temp;
-        }else if(raiz->esq == NULL){
-            No* temp = raiz->dir;
+        } else if(raiz->esq == NULL){
+            No *temp = raiz->dir;
             free(raiz);
             return temp;
         }
-
         int maiorValor = maiorEsq(raiz->esq);
         raiz->valor = maiorValor;
         raiz->esq = removerRec(raiz->esq, maiorValor);
     }
-
     return raiz;
-
-
 }
 
+void remover(Arvore *arvore, int x){
+    arvore->raiz = removerRec(arvore->raiz, x);
+}
